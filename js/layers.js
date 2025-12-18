ece7bc5c67c57a5e7ad394d98e7a31054b0b42c8
PLMMJ-FH <plmmj2011@gmail.com>
2025-12-18 18:25:02 -0500

    Making UE easier to get
    Slightly reduced the cost of upgrade 21.

--- js/layers.js
+++ js/layers.js
@@ -88,7 +88,7 @@ addLayer("u", {
         21: {
             title: "I swear this isn't a prestige reskin",
             description: "Allows generation of upgrade essence.",
-            cost: new Decimal(25),
+            cost: new Decimal(20),
             unlocked() { return hasUpgrade("u", 12)&&hasUpgrade("u", 13) },
         },
         22: {
