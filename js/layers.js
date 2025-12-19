f9411a37c4a476a53a0f063811261ed2739f4e74
PLMMJ-FH <plmmj2011@gmail.com>
2025-12-18 19:39:45 -0500

    UE softcap
    UE gain is now cube rooted if it surpasses 1e12

--- js/layers.js
+++ js/layers.js
@@ -23,6 +23,7 @@ addLayer("u", {
 	effect() { // calculates UE gain
         if (!hasUpgrade('u', 21)) return new Decimal(0);
         let eff = Decimal.pow(this.effBase(), player.u.points).sub(1).max(0);
+        if (player.u.essence.gte(1e12)) eff = eff.pow(1/3)
         return eff;
     },
 	effectDescription() { // text for UE gain
