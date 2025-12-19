9f38f525fb4ad40ac13f9bbabaec1f4ee1d90ba8
PLMMJ-FH <plmmj2011@gmail.com>
2025-12-18 23:46:00 -0500

    Harder milestones
    Milestones scale faster.

--- js/layers.js
+++ js/layers.js
@@ -143,7 +143,7 @@ addLayer("m", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     branches: ["p"],
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1.5, // Prestige currency exponent
+    exponent: 2, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
